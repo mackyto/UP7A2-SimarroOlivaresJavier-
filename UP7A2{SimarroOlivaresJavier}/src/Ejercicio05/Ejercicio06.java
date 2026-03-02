b465b25ab20274a1f3b9ad851863bfd46c4e6bfe
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2026-03-02 13:26:03 +0100

    comenzando ejercicio 01

--- UP7A2{SimarroOlivaresJavier}/src/Ejercicio05/Ejercicio06.java
+++ UP7A2{SimarroOlivaresJavier}/src/Ejercicio05/Ejercicio06.java
@@ -53,8 +53,5 @@ public class Ejercicio06 {
         return inverso;
         
     }
-   
-    
-    
     
 }
---
+++ UP7A2{SimarroOlivaresJavier}/src/Ejercicio06/Ejercicio06.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio06;
+
+/**
+ *
+ * Implementa un programa que gestione una cola de espera para las atracciones de PortAventura. 
+ * Cada persona en la cola tendrá un nombre, edad y altura (en cm). 
+ * 
+ * El programa debe permitir:
+ *
+ *  • Añadir personas a la cola.
+ *  • Permitir el acceso (eliminar) a la siguiente persona si cumple los requisitos de la atracción.
+ *  • Mostrar toda la cola de espera. 
+ *  • Mostrar el tiempo estimado de espera (3 minutos por persona en la cola). 
+ *  • Verificar si una persona cumple los requisitos mínimos para la atracción 
+ *    (por ejemplo, altura mínima de 120cm y edad mínima de 10 años)
+ * 
+ * @author macky
+ */
+public class Ejercicio06 {
+    
+    
+    
+    
+    
+}
