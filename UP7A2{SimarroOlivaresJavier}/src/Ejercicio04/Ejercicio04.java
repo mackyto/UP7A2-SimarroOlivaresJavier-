14049e5e6210ac7acb35eb105895b31973a1b734
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2026-03-02 01:55:38 +0100

    comenzando ejercicio 01

---
+++ UP7A2{SimarroOlivaresJavier}/src/Ejercicio04/Ejercicio04.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio04;
+
+/**
+ *
+ * Desarrolla un programa que simule una cola de clientes en un supermercado. 
+ * Cada cliente tendrá un nombre y un número de artículos. El programa debe 
+ * permitir añadir clientes a la cola y atender (eliminar) al primer cliente.
+ * Además, deberá ofrecer un tiempo estimado de espera, 
+ * considerando un tiempo de 2 segundos por cada artículo de cada cliente
+ * 
+ * @author macky
+ */
+public class Ejercicio04 {
+    
+}
