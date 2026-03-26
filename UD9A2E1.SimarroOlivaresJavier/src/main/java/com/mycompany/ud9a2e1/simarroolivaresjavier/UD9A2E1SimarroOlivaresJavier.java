66c53b109039b48f96611aca3a2998579ac63d8f
Javier Simarro Olivares <jsimarro@me.com>
2026-03-26 20:22:30 +0100

    envío a Github

---
+++ UD9A2E1.SimarroOlivaresJavier/src/main/java/com/mycompany/ud9a2e1/simarroolivaresjavier/UD9A2E1SimarroOlivaresJavier.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ud9a2e1.simarroolivaresjavier;
+
+/**
+ *
+ * @author macky
+ */
+public class UD9A2E1SimarroOlivaresJavier {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
